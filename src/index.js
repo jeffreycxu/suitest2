adfd5b57a8b3953d368ca6a6a25dfcbc9ae11d6c
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-24 14:38:56 -0700

    adding ethosConfiguration

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { EthosConnectProvider } from 'ethos-connect';
+import { EthosConnectProvider } from 'ethos-connect';
+// import { NETWORK } from "./lib/constants";
+
+const ethosConfiguration = {
+  apiKey: process.env.NEXT_PUBLIC_ETHOS_API_KEY,
+  preferredWallets: ['Ethos Wallet'],
+  network: process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK
+
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <EthosConnectProvider>
-    <App />
-  // </EthosConnectProvider>
+  <EthosConnectProvider
+  ethosConfiguration={ethosConfiguration}
+  dappName="EthosConnect Example App"
+  connectMessage="Your connect message goes here!"
+>    <App />
+  </EthosConnectProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const NETWORK = process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK
+export const FAUCET = process.env.FAUCET || process.env.NEXT_PUBLIC_FAUCET
+export const ETHOS_EXAMPLE_CONTRACT = "0x9877e0cac9acceb710e1308f3b0e129fc8a2f437fe2fa0505b9dc95eeafbf0ce"
+export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0x3c7f933947a250063299b9623ed41ba412b06577e6f150f713d87f3eef725"
+export const ETHOS_COIN_TYPE = `${ETHOS_EXAMPLE_CONTRACT}::ethos_example_coin::ETHOS_EXAMPLE_COIN`
