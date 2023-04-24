c4e6eb4a041c47b2c9aec647d0c9dbd89f772b38
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-24 13:45:08 -0700

    removing provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { EthosConnectProvider } from 'ethos-connect';
+// import { EthosConnectProvider } from 'ethos-connect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <EthosConnectProvider>
+  // <EthosConnectProvider>
     <App />
-  </EthosConnectProvider>
+  // </EthosConnectProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
