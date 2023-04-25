9ba4bcd3de990b0854d943f9d9bf4e8001d9608c
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-24 17:35:08 -0700

    removing reportwebvitals

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { EthosConnectProvider } from 'ethos-connect';
-// import { NETWORK } from "./lib/constants";
+// import { NETWORK } from "./lib/cons  tants";
 
 const ethosConfiguration = {
   apiKey: process.env.NEXT_PUBLIC_ETHOS_API_KEY,
   preferredWallets: ['Ethos Wallet'],
   network: process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK
-
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,8 +21,3 @@ root.render(
 >    <App />
   </EthosConnectProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
