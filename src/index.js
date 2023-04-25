48c8f13f3a27a05cb256fc589d6dd837ec1dc8f1
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-24 18:03:36 -0700

    fix spacing

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { EthosConnectProvider } from 'ethos-connect';
-// import { NETWORK } from "./lib/cons  tants";
 
 const ethosConfiguration = {
   apiKey: process.env.NEXT_PUBLIC_ETHOS_API_KEY,
   preferredWallets: ['Ethos Wallet'],
   network: process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK
 };
+
 console.log(ethosConfiguration, "test")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <EthosConnectProvider
-  ethosConfiguration={ethosConfiguration}
-  dappName="EthosConnect Example App"
-  connectMessage="Your connect message goes here!"
->    <App />
+    ethosConfiguration={ethosConfiguration}
+    dappName="EthosConnect Example App"
+    connectMessage="Your connect message goes here!"
+  >   
+    <App />
   </EthosConnectProvider>
 );
