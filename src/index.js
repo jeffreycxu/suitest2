6fcf9e7c99799fc0817900324f5329e5dd329e10
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-24 17:39:05 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { EthosConnectProvider } from 'ethos-connect';
 // import { NETWORK } from "./lib/cons  tants";
 
