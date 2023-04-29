61811066cae99e57a001a5201bf858c569f2d7c1
Jeffrey Xu <xujeffrey.c@gmail.com>
2023-04-29 06:50:37 -0700

    adding ethos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
-// import { SignInButton } from 'ethos-connect';
+import { SignInButton } from 'ethos-connect';
 
 
 function App() {
   return (
     <div className="App">
       <p> Testing</p>
-      {/* <SignInButton/> */}
+      <SignInButton/>
 
     </div>
   );
